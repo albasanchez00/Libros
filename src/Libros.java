978ed9d4cb99477b499d7d641be3bb6ba79df49e
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-04-22 20:47:46 +0200

    commit main

--- src/Libros.java
+++ src/Libros.java
@@ -1,4 +1,80 @@
-package PACKAGE_NAME;
-
 public class Libros {
+    private String titulo,autor,editorial,isbn;
+    private int anioPubli, paginas;
+    private int idLibro;
+    private static int idContar=0;
+
+    public Libros(String titulo, String autor, String editorial, String isbn, int anioPubli, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.isbn = isbn;
+        this.anioPubli = anioPubli;
+        this.paginas = paginas;
+        this.idLibro = ++idContar;
+    }
+
+    //Métodos Getter y Setter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnioPubli() {
+        return anioPubli;
+    }
+
+    public void setAnioPubli(int anioPubli) {
+        this.anioPubli = anioPubli;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public int getIdLibro() {
+        return idLibro;
+    }
+
+    @Override
+    public String toString() {
+        return "\nLibro  ID ("+ idLibro +")" +
+                "\nTitulo → " + titulo +
+                "\nAutor → " + autor +
+                "\nEditorial → " + editorial +
+                "\nISBN → " + isbn +
+                "\nAño publicación → " + anioPubli +
+                "\nPáginas → " + paginas;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,187 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/* Recrear lo que hemos realizado ayer con Alumnos, crearlo en un nuevo proyecto usando
+una Clase Libro.
+Atributos
+id
+Titulo
+Autor
+Paginas
+ISBN
+Año Publicacion
+Editorial
+* */
+
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        menu();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static ArrayList<Libros> grupoLibros=new ArrayList();
+
+    public static void menu(){
+        int opcion=0;
+        String opcionString= JOptionPane.showInputDialog(null,"Ingrese la opcion: \n1. Nuevo Libro \n2. Eliminar \n3. Modificar \n4. Mostrar \n5. Salir");
+        try{
+            opcion=Integer.parseInt(opcionString);
+            switch (opcion) {
+                case 1,3-> crearModificar(opcion);
+                case 2-> eliminar();
+                case 4-> mostrar();
+                case 5-> {
+                    JOptionPane.showMessageDialog(null, "Saliendo...");
+                    break;
+                }
+                default-> JOptionPane.showMessageDialog(null,"Ingrese una opción válida");
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Ingrese una opción valida");
         }
+    }
+
+
+
+    public static void crearModificar(int opcion){
+        String titulo="",autor="", editorial="",isbn="";
+        int anioPubli=0, paginas=0;
+        if (opcion==1){
+            titulo = JOptionPane.showInputDialog(null, "Ingrese el título del libro: ");
+            autor = JOptionPane.showInputDialog(null, "Ingrese el autor del libro: ");
+            editorial = JOptionPane.showInputDialog(null, "Ingrese la editorial: ");
+            isbn = JOptionPane.showInputDialog(null, "Ingrese el código ISBN: ");
+            anioPubli = leerFecha();
+            paginas = leerPaginas();
+
+            Libros nuevo=new Libros(titulo,autor,editorial,isbn,anioPubli,paginas);
+            if (nuevo != null) {
+                grupoLibros.add(nuevo);
+                menu();
+            }
+        }
+
+        if (opcion==3){
+            String salida=mostrarLibros();
+            String modificarId=JOptionPane.showInputDialog(null,"Ingrese el id del libro: ");
+            String queModificar=JOptionPane.showInputDialog(null,"1.Título \n2.Autor \n3.Editorial \n4. ISBN \n5. Año Publicación \n6.Páginas");
+            int opcionModificar=0;
+            int idLibroMod=0;
+            try {
+                opcionModificar=Integer.parseInt(queModificar);
+                idLibroMod=Integer.parseInt(modificarId);
+                switch (opcionModificar){
+                    case 1->{
+                        titulo = JOptionPane.showInputDialog(null, "Ingrese el nuevo título → ");
+                    }
+                    case 2->{
+                        autor = JOptionPane.showInputDialog(null, "Ingrese el nuevo apellido → ");
+                    }
+                    case 3->{
+                        editorial = JOptionPane.showInputDialog(null, "Ingrese la nueva editorial → ");
+                    }
+                    case 4->{
+                        isbn = JOptionPane.showInputDialog(null, "Ingrese el nuevo ISBN → ");
+                    }
+                    case 5->{
+                        paginas =leerPaginas();
+                    }
+                    case 6->{
+                        anioPubli = leerFecha();
+                    }
+                }
+                Iterator<Libros> librosIterator=grupoLibros.iterator();
+                while (librosIterator.hasNext()){
+                    Libros libro = librosIterator.next();
+                    if (libro.getIdLibro() == idLibroMod){
+                        switch(opcionModificar) {
+                            case 1 -> libro.setTitulo(titulo);
+                            case 2 -> libro.setAutor(autor);
+                            case 3 -> libro.setEditorial(editorial);
+                            case 4 -> libro.setIsbn(isbn);
+                            case 5 -> libro.setPaginas(paginas);
+                            case 6 -> libro.setAnioPubli(anioPubli);
+                        }
+                        JOptionPane.showMessageDialog(null, "Libro modificado exitosamente");
+                        break;
+                    }
+                }
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null,"No exiten libros que mostrar");
+            }
+            mostrar();
+        }
+    }
+
+
+    public static void eliminar(){
+        if (!grupoLibros.isEmpty()){
+            String salida="";
+            String idEliminar=JOptionPane.showInputDialog(null,"Ingrese el id del libro a eliminar → ");
+            int id;
+            boolean eliminado=false;
+            try {
+                id=Integer.parseInt(idEliminar);
+                Iterator <Libros> it = grupoLibros.iterator();
+                while (it.hasNext()){
+                    Libros libro = it.next();
+                    if (libro.getIdLibro()==id){
+                        it.remove();
+                        eliminado=true;
+                    }
+                }
+                if (!eliminado){
+                    JOptionPane.showMessageDialog(null,"El id del libro no existe");
+                }
+                mostrar();
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null,"Ingrese el id del libro a eliminar");
+            }
+        }else {
+            mostrar();
+        }
+    }
+
+    public static void mostrar(){
+        String salida= mostrarLibros();
+        JOptionPane.showMessageDialog(null,salida);
+        menu();
+    }
+
+    public static String mostrarLibros(){
+        String salida="";
+        if (!grupoLibros.isEmpty()){
+            for (Libros libro:grupoLibros){
+                salida+=libro.toString();
+            }
+        }else {
+            salida="Libro no encontrado";
+        }
+        return salida;
+    }
+
+    public static int leerFecha(){
+        int anioPubli=0;
+        String anioString="";
+        anioString=JOptionPane.showInputDialog(null ,"Ingrese la fecha de publicación");
+        try {
+            anioPubli= Integer.parseInt(anioString);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Ingrese un número entero, sin decimales");
+        }
+        return anioPubli;
+    }
+
+    public static int leerPaginas(){
+        int numPaginas=0;
+        String paginaString="";
+        paginaString = JOptionPane.showInputDialog(null,"Ingrese el número de páginas");
+        try {
+            numPaginas= Integer.parseInt(paginaString);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Ingrese un número entero, sin decimales");
+        }
+        return numPaginas;
     }
 }
